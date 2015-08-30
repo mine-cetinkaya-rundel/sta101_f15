226b3ce5f67ed30bc8ca31a7ff1ff9e2ea588b34
mine-cetinkaya-rundel <mine@stat.duke.edu>
2015-08-30 17:20:28 -0400

    update app exes for unit 1 and add solutions

--- Application Exercises/1.3 Histogram to boxplot/figures/survey.R
+++ Application Exercises/1.3 Histogram to boxplot/figures/survey.R
@@ -1,16 +1,27 @@
+# load libraries ----------------------------------------------------
+library(ggplot2)
 library(openintro)
 
-# load data
-d <- read.csv("~/Desktop/Teaching/Sta101_S15/Class survey/surveyS15_1_13.csv")
+# openintro colors --------------------------------------------------
+data(COL)
 
-# histogram of university applications
-pdf("hist_university_applications.pdf", width = 7, height = 4)
-par(mar = c(3.2,2,1.5,0.5), mgp = c(2,1,0))
-hist(d$university_applications, col = COL[1], main = "How many universities did you apply to?", xlab = "universities", ylab = "", cex.main = 1, las = 1, ylim = c(0,25))
-abline(h = seq(0, 25, 5), col = COL[6,2], lty = 2)
-abline(h = seq(0, 25, 1), col = COL[6,2], lty = 3)
-dev.off()
+# load data ---------------------------------------------------------
+d <- read.csv("surveyS15_univ_app.csv")
 
-# no NAs
+# histogram of university applications ------------------------------
+
+hist_uni_app <- ggplot(d, aes(x = university_applications)) +
+  geom_histogram(binwidth = 2, fill = COL[1,2], col = COL[1]) +
+  scale_y_continuous(breaks = seq(0, 30, 2)) +
+  scale_x_continuous(breaks = seq(0, 24, 4), limits = c(0, 24)) +
+  xlab("Number of university applications") +
+  ylab("Frequency")
+                     
+ggsave(hist_uni_app, file = "hist_university_applications.pdf", 
+       width = 7, height = 4)
+
+# summary stats -----------------------------------------------------
+# note: no NAs
 summary(d$university_applications)
 length(d$university_applications)
+
--- Application Exercises/1.4 Randomization testing/ratio/ratio.R
+++ Application Exercises/1.4 Randomization testing/ratio/ratio.R
@@ -1,33 +1,52 @@
-setwd("~/Desktop/Teaching/Sta 104 - Su13/Slides/(1) Unit 1/Lec 3/figures/ratio")
-      
+# load packages -----------------------------------------------------
 library(openintro)
-rm(list=ls())
+library(dplyr)
+library(xtable)
 
+# load data ---------------------------------------------------------
 d = read.csv("teach_stu_rat.csv")
+
+# format data -------------------------------------------------------
+# round student-faculty ratio
+# relabel public / private
+
 d$rat = round(d$studentfacultyratio, 2)
 d$type[d$Private == 1] = "private"
 d$type[d$Private == 0] = "public"
 d$type = factor(d$type, levels = c("private", "public"))
 
-by(d$rat, d$type, mean)
-by(d$rat, d$type, sd)
-by(d$rat, d$type, length)
+# summary stats -----------------------------------------------------
+sum_tab <- d %>%
+  group_by(type) %>%
+  summarize(mean = round(mean(rat), 2), SD = round(sd(rat), 2), n = n())
 
+print(xtable(sum_tab), include.rownames = FALSE)
 
-pdf("ratio_box.pdf", width = 6, height = 3)
-par(mar = c(3,4.2,0.5,0.5))
-boxplot(d$rat ~ d$type, horizontal = TRUE, axes = FALSE)
-axis(1, cex.axis = 1.25)
-axis(2, at = c(1,2), levels(d$type), cex.axis = 1.25, las = 1)
-dev.off()
+# side-by-side box plots of ratio vs. type ---------------------------
+ratio_box <- ggplot(data = d, aes(x = factor(type), y = rat)) +
+  geom_boxplot() +
+  xlab("Type of school") +
+  ylab("Student to faculty ratio")
 
-set.seed(1345)
-rand_dist = inference(d$rat, d$type, est = "mean", type = "ht", method = "simulation", null = 0, alternative = "greater", order = c("public","private"), nsim = 100, simdist = TRUE)
+ggsave(ratio_box, file = "ratio_box.pdf", width = 6, height = 3)
+
+# load inference function -------------------------------------------
+
+download.file("https://stat.duke.edu/~mc301/R/inference.RData", destfile = "inference.RData")
+load("inference.RData")
+
+# randomization -----------------------------------------------------
+
+rand_dist = inference(d$rat, d$type, est = "mean", type = "ht", method = "simulation", null = 0, alternative = "greater", order = c("public","private"), nsim = 100, simdist = TRUE, seed = 123456)
 rand_dist[which.max(rand_dist)] = 4.1
-rand_dist[which.min(rand_dist)] = -4.08
-rand_dist[50] = -3
 
-pdf("rand_dist.pdf", width = 4, height = 2)
-par(mar = c(4,0,0,0))
-BHH2::dotPlot(rand_dist, xlim = c(-4.5,4.5), pch = 20, cex = 0.8, xlab = "")
-dev.off()
+rand_df <- data.frame(rand_dist = rand_dist)
+
+rand_dist_dot <- ggplot(data = rand_df, aes(x = rand_dist)) +
+  geom_dotplot(binwidth = 0.2) +
+  xlab("Distribution of simulated differences (public - private)") +
+  scale_x_continuous(limits = c(-5, 5), breaks = seq(-5, 5, 1)) +
+  theme(axis.title.y = element_blank(), axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
+
+ggsave(rand_dist_dot, file = "rand_dist_dot.pdf", width = 5, height = 2)
